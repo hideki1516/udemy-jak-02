f91adba3b93389550e06e8b2580923e116cc65ed
hideki <hideki.1516.web@gmail.com>
2022-03-10 18:18:03 +0900

    state-pass-over

--- src/About.jsx
+++ src/About.jsx
@@ -1,14 +1,16 @@
 import { Link } from 'react-router-dom';
 
 export const About = () => {
+  const message = 'Corporateページです'
+
   return (
     <div>
       <h1>Aboutページです</h1>
-      <Link to='/about/corporate'>Corporate</Link>
+      <Link to={{pathname: '/about/corporate', state: message}}>Corporate</Link>
       <br />
-      <Link to='/about/access'>Access</Link>
+      <Link to={{pathname: '/about/access'}}>Access</Link>
       <br />
-      <Link to='/about/about-id/999?name=hagehage'>AboutId</Link>
+      <Link to={{pathname: '/about/about-id/999?name=hagehage'}}>AboutId</Link>
     </div>
   );
 };
