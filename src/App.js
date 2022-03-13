9f9bed92e8257346a3140c542eb34ee311927c54
hideki <hideki.1516.web@gmail.com>
2022-03-13 23:29:54 +0900

    Template

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
 import './App.css';
 import { PrimaryButton } from './components/atoms/button/PrimaryButton';
 import { SecondaryButton } from './components/atoms/button/SecondaryButton';
 import { SearchInput } from './components/molecules/SearchInput';
 import { Product } from './components/organisms/product/Product';
 import { UserCard } from './components/organisms/user/UserCard';
+import { DefaultLayout } from './components/templates/DefaultLayout';
+import { HeaderOnly } from './components/templates/HeaderOnly';
 
 // 子コンポーネントに渡すpropsを設定
 const user = {
@@ -27,16 +30,18 @@ const product = {
 
 function App() {
   return (
-    <div className="App">
-      <PrimaryButton>テスト</PrimaryButton>
-      <SecondaryButton>検索</SecondaryButton>
-      <br />
-      <SearchInput />
-      <UserCard user={user} />
-      <br />
-      <br />
-      <Product product={product} />
-    </div>
+    <BrowserRouter>
+    <DefaultLayout>
+        <PrimaryButton>テスト</PrimaryButton>
+        <SecondaryButton>検索</SecondaryButton>
+        <br />
+        <SearchInput />
+        <UserCard user={user} />
+        <br />
+        <br />
+        {/* <Product product={product} /> */}
+      </DefaultLayout>
+    </BrowserRouter>
   );
 }
 
