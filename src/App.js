626344b40ac160d7ca42ca47ca6e8e0948c081e5
hideki <hideki.1516.web@gmail.com>
2022-03-20 10:54:17 +0900

    43-JSONPlaceholder-axios

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const onClickUsers = () => {
+    // get()：データを取得
+    // then()：取得後の関数を設定できる／引数には取得したデータの情報が入る
+    // オブジェクトの値を指定することで中身を参照できる
+    // catch()：エラーが起きたときの関数を設定できる
+    axios.get('https://jsonplaceholder.typicode.com/users').then((result) => {
+      console.log(result.data);
+    }).catch((error) => console.log(error));
+  };
+
+  const onClickUser1 = () => {
+    axios.get('https://jsonplaceholder.typicode.com/users/3').then((result) => {
+      console.log(result.data);
+    }).catch((error) => console.log(error));
+  }
+
+  const onClickPhotos = () => {
+    axios.get('https://jsonplaceholder.typicode.com/photos/1').then((result) => {
+      console.log(result.data.url);
+    }).catch((error) => console.log(error));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={onClickUsers}>users</button>
+      <button onClick={onClickUser1}>id=1のuser</button>
+      <button onClick={onClickPhotos}>album</button>
     </div>
   );
 }
